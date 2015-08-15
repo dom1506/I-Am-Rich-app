93c457710988522895e684f1067744a0ba4dec46
Mr.Bi <MrBi@Bis-MacBook-Pro.local>
2015-08-15 11:33:42 +0700

    Initial Commit

---
+++ IAmRich/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IAmRich
+//
+//  Created by Mr.Bi on 8/15/15.
+//  Copyright (c) 2015 Mr.Bi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IAmRich/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IAmRich
+//
+//  Created by Mr.Bi on 8/15/15.
+//  Copyright (c) 2015 Mr.Bi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
